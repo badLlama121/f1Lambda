2f14f9e51930fc326e9d0374e43511f7ee3eae25
jecklt <kevin.theisen26@gmail.com>
2024-05-06 19:42:27 -0500

    working code to produce csv

--- data.py
+++ data.py
@@ -1,12 +1,17 @@
 import fastf1
+import csv
 
-session = fastf1.get_session(2024, 'china', 'race', backend='fastf1')
+race = 'miami'
+
+session = fastf1.get_session(2024, race, 'race')
 session.load(telemetry=False, laps=False, weather=False)
-
-def process_first_round(){
-
-
-    
-}
-
+drivers = session.drivers
 print (session.results)
+
+print(session.get_driver('VER'))
+
+with open('results.csv', 'w') as file:
+        file.writelines('Driver,Round1 Points, Round2 Points,Round3 Points\n')
+        for driver in drivers:
+            row = (session.get_driver(driver).BroadcastName + ',' + str(int(session.get_driver(driver).Points)) + ',' + str(int((session.get_driver(driver).Points))*2) + ',' + str((session.get_driver(driver).GridPosition - session.get_driver(driver).Position )) + '\n')
+            file.writelines(row)
